9ebfff660dca41c8d6825d8567eb3aa789902fcf
nicolasperivolaris <perivolarisnicolas@yahoo.fr>
2022-02-24 14:55:50 +0100

    Bug fix
    Bonne réponse

--- ErathostenesDyna.c
+++ ErathostenesDyna.c
@@ -37,7 +37,7 @@ int main(int argc, char* argv[]) {
 	ListPrimes listPrimes;
     do {
         InitializeTListPrimes(&listPrimes);
-        listPrimes.maximum = ReadIntLimited("\nVotre naturel maximum ? ", 0, MAX);
+        listPrimes.maximum = 20;//ReadIntLimited("\nVotre naturel maximum ? ", 0, MAX);
         ct1 = clock();
         Erathostenes(&listPrimes);
         ct2 = clock();
@@ -59,16 +59,16 @@ void InitializeTListPrimes(ListPrimes *list){
 	list->maximum = 0;
 }
 
-int* toIntArray(uint32* tab, int size){
-	int* result = malloc(sizeof(int)*size);
+void toIntArray(uint32* tab, ListPrimes *list){	
+	int* result = malloc(sizeof(int)*list->cPrimes);
 	int j = 0;
-	for(int i=0; i<size; i++){
-		if(issetbitarray(tab, i)){
+	for(int i=0; (j<list->cPrimes) | (i<list->maximum); i++){
+		if(!issetbitarray(tab, i)){ 
 			result[j] = i;
 			j++;
 		}
 	}
-	return result;
+	list->pPrimes = result;
 }
 
 void fillArrayWith(uint32 *tab, int size, int value){
@@ -78,26 +78,29 @@ void fillArrayWith(uint32 *tab, int size, int value){
 }
 
 void Erathostenes(ListPrimes *list){
-	int limit = sqrt(list->maximum);
-	uint32 tab[limit/32];
-	fillArrayWith(tab, limit/32, MAX); //fill array with 1 bits
-	unsetbitarray(tab, 0);
-	unsetbitarray(tab, 1);
-	for(int i=2; i<limit; i++){//read the tab until limit
-		if(issetbitarray(tab, i)){//check next prime
-			for(int j = i*i; j<limit; j+=i){//delete multiples
-				unsetbitarray(tab,j);
+	int limit = (list->maximum-1)/32 +1;
+	uint32 *tab = (uint32*)calloc(limit, sizeof(uint32));
+	//if prime -> 0, if not -> 1
+	//fillArrayWith(tab, limit, 0); //fill array with 0 bits
+	setbitarray(tab, 0); 
+	setbitarray(tab, 1);
+	uint32 notPrime = 0;
+	for(int i=2; i*i<=list->maximum; i++){//read the tab until limit
+		if(!issetbitarray(tab, i)){//if not already checked
+			for(int j = i*i; j<list->maximum; j+=i){//uncheck multiples
+				setbitarray(tab,j);
+				notPrime++;
 			} 
 		}
 	}
-	
-	list->pPrimes = toIntArray(tab, limit);
+	list->cPrimes = list->maximum - notPrime;
+	toIntArray(tab, list);
 }
 
 void ShowPrimes(ListPrimes *list){
-	printf("Il y a %d nombres premiers inférieurs à %d", list->cPrimes, list->maximum);
-	for(int i=0; i<list->maximum; i++){
-		printf("Nombre premier n %d: %d\n",i, list->pPrimes[i]);
+	printf("Il y a %d nombres premiers inferieurs a %d\n", list->cPrimes, list->maximum);
+	for(int i=0; i<list->cPrimes; i++){
+		printf("Nombre premier n%d: %d\n",i, list->pPrimes[i]);
 	}
 	Stop();
 }
